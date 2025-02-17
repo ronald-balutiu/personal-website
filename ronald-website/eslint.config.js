27b819c2f0759c632816c028ae59ef0afece8a2a
Ronald Balutiu <rbalutiu@gmail.com>
2025-02-17 05:11:24 -0500

    Footer looks good +  other css

--- ronald-website/eslint.config.js
+++ ronald-website/eslint.config.js
@@ -1,22 +1,22 @@
 export default [
-    {
-        ignores: ["node_modules/", "dist/", ".astro/"],
+  {
+    ignores: ['node_modules/', 'dist/', '.astro/'],
+  },
+  {
+    extends: ['eslint:recommended', 'plugin:astro/recommended', 'plugin:prettier/recommended'],
+    plugins: ['astro', 'prettier'],
+    rules: {
+      'prettier/prettier': 'error',
     },
-    {
-        extends: ["eslint:recommended", "plugin:astro/recommended", "plugin:prettier/recommended"],
-        plugins: ["astro", "prettier"],
-        rules: {
-            "prettier/prettier": "error",
+    overrides: [
+      {
+        files: ['*.astro'],
+        parser: 'astro-eslint-parser',
+        parserOptions: {
+          parser: '@typescript-eslint/parser',
+          extraFileExtensions: ['.astro'],
         },
-        overrides: [
-            {
-                files: ["*.astro"],
-                parser: "astro-eslint-parser",
-                parserOptions: {
-                    parser: "@typescript-eslint/parser",
-                    extraFileExtensions: [".astro"],
-                },
-            },
-        ],
-    },
-];
+      },
+    ],
+  },
+]
